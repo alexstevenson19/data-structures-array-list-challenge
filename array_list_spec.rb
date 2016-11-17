5f8ce9525efffc64dea1bf82318029098f12831a
Alex Stevenson <alexstevenson19@gmail.com>
2016-11-16 23:22:39 -0600

    initial commit, pseudocode

--- array_list_spec.rb
+++ array_list_spec.rb
@@ -1,5 +1,31 @@
 require_relative 'array_list'
 
 describe ArrayList do
+	let(:new_list) { ArrayList.new(3) }
 
+
+	# test for ArrayList.new(size)
+
+	# test for ArrayList.add(element)
+
+	# test for ArrayList.get(index)
+
+	# test for ArrayList.set(element, index)
+
+	# test for ArrayList.length
 end
+
+
+
+	# let(:new_array) { FixedArray.new(3) }
+
+	# describe "fixed array size" do
+	# 	it "should have a set length" do
+	# 		# p new_array.array.length
+	# 		expect(new_array.array.length).to eq(3)
+	# 	end
+
+	# 	it "should not be a different length" do
+	# 		expect(new_array.array.length).not_to eq(4)
+	# 	end
+	# end
